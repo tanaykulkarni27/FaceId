6a9ba9a4c78e13e655e9476dfdb5ef324b66c3c7
tanaykulkarni27 <1002kulkarnitp@gmail.com>
2022-01-19 15:41:15 +0530

    updated facerecog module

--- FaceRecog/Base.py
+++ FaceRecog/Base.py
@@ -1,22 +1,22 @@
 import cv2
-from PIL import Image
+from pathlib import Path
+import os 
 class FaceHandler:
     def __init__(self,img):
         self.img = img
-        self.CASCADE_PATH = "C:\\Users\\Tanay\\AppData\\Roaming\\Python\\Python310\\site-packages\\cv2\\data\\haarcascade_frontalface_alt2.xml"
+        self.MODULE_DIR = Path(__file__).resolve().parent
+        self.CASCADE_DIR = os.path.join(self.MODULE_DIR,'cascades/data/haarcascade_frontalface_alt2.xml')
     def get_face_from_image(self):
         img = self.img
-        face_cascade = cv2.CascadeClassifier(self.CASCADE_PATH)
+        face_cascade = cv2.CascadeClassifier(self.CASCADE_DIR)
         gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        x = Image.fromarray(gray_image)
         cnt = 0;
         face = None
-        faces = face_cascade.detectMultiScale(gray_image,1.3,1) # return face dimensions 
+        faces = face_cascade.detectMultiScale(gray_image,1.3,1) # returns list of faces
         for x,y,w,h in faces: 
             if cnt >= 1:
-                assert cnt <= 0,"Image should contain single image"
-            face_part = gray_image[y : y+h,x:x + w] # slices the faces 
+                assert cnt <= 0,"Image should contain single face"
+            face_part = gray_image[y : y+h,x:x + w] # slices the faces from images
             face = face_part
             cnt += 1
         return face
-# get_face_from_image(None)
